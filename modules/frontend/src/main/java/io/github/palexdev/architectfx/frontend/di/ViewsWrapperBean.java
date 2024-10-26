03119c6299f522391e7cf2ef23c1f405ab76b72b
palexdev <alessandro.parisi406@gmail.com>
2024-10-26 11:25:36 +0200

    [frontend] :sparkles: Finish implementing ViewsWrapperBean

--- modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/di/ViewsWrapperBean.java
+++ modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/di/ViewsWrapperBean.java
@@ -1,8 +1,10 @@
 package io.github.palexdev.architectfx.frontend.di;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import io.github.palexdev.architectfx.frontend.views.base.View;
 import io.inverno.core.annotation.Bean;
@@ -14,8 +16,12 @@ import io.inverno.core.annotation.Wrapper;
 public class ViewsWrapperBean implements Supplier<Map<Class<? extends View>, View<?>>> {
     private final Map<Class<? extends View>, View<?>> views;
 
-    public ViewsWrapperBean() {
-        this.views = new HashMap<>();
+    public ViewsWrapperBean(List<View<?>> views) {
+        this.views = views.stream()
+            .collect(Collectors.toMap(
+                View::getClass,
+                Function.identity()
+            ));
     }
 
     @Override
