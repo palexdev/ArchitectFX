569cbec782839a002e8fd0b946ba84ce17a20e51
palexdev <alessandro.parisi406@gmail.com>
2024-10-26 11:40:28 +0200

    [frontend] :recycle: Make ViewManager responsible for initializing and showing the main window

--- modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/views/ViewManager.java
+++ modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/views/ViewManager.java
@@ -2,11 +2,17 @@ package io.github.palexdev.architectfx.frontend.views;
 
 import java.util.Map;
 
+import io.github.palexdev.architectfx.frontend.ArchitectFX;
+import io.github.palexdev.architectfx.frontend.events.AppEvent;
 import io.github.palexdev.architectfx.frontend.events.UIEvent;
+import io.github.palexdev.architectfx.frontend.settings.AppSettings;
 import io.github.palexdev.architectfx.frontend.views.base.View;
 import io.github.palexdev.mfxcore.events.bus.IEventBus;
 import io.inverno.core.annotation.Bean;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 @Bean
 @SuppressWarnings("rawtypes")
@@ -14,22 +20,47 @@ public class ViewManager {
     //================================================================================
     // Properties
     //================================================================================
+    private final Stage mainWindow;
     private final Pane rootPane;
     private final Map<Class<? extends View>, View<?>> views;
+    private final AppSettings settings;
 
     //================================================================================
     // Constructors
     //================================================================================
-    public ViewManager(Pane rootPane, Map<Class<? extends View>, View<?>> views, IEventBus events) {
+    public ViewManager(
+        Stage mainWindow,
+        Pane rootPane,
+        Map<Class<? extends View>, View<?>> views, IEventBus events,
+        AppSettings settings
+    ) {
+        this.mainWindow = mainWindow;
         this.rootPane = rootPane;
         this.views = views;
+        this.settings = settings;
+        events.subscribe(AppEvent.AppReadyEvent.class, e -> onAppReady());
         events.subscribe(UIEvent.ViewSwitchEvent.class, this::onViewSwitchRequest);
     }
 
     //================================================================================
     // Methods
     //================================================================================
-    protected void onViewSwitchRequest(UIEvent.ViewSwitchEvent event) {
+    private void initMainWindow() {
+        Scene scene = new Scene(rootPane);
+        mainWindow.setScene(scene);
+        mainWindow.initStyle(StageStyle.UNIFIED);
+        onViewSwitchRequest(new UIEvent.ViewSwitchEvent(InitView.class));
+        mainWindow.show();
+    }
+
+    private void onAppReady() {
+        initMainWindow();
+        mainWindow.sizeToScene();
+        mainWindow.centerOnScreen();
+        mainWindow.setResizable(false);
+    }
+
+    private void onViewSwitchRequest(UIEvent.ViewSwitchEvent event) {
         View<?> view = views.get(event.data());
         if (view == null)
             throw new IllegalStateException("Unknown view: " + event.data());
