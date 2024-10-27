577978562fbce6e2b9bc257aa05d88d425c9a801
palexdev <alessandro.parisi406@gmail.com>
2024-10-27 23:11:01 +0100

    [frontend] :wrench: Update module info

--- modules/frontend/src/main/java/module-info.java
+++ modules/frontend/src/main/java/module-info.java
@@ -29,11 +29,36 @@ module afx.frontend {
     // Base Package
     exports io.github.palexdev.architectfx.frontend;
 
+    // Components Package
+    exports io.github.palexdev.architectfx.frontend.components;
+    exports io.github.palexdev.architectfx.frontend.components.base;
+    exports io.github.palexdev.architectfx.frontend.components.selection;
+    exports io.github.palexdev.architectfx.frontend.components.vfx;
+    exports io.github.palexdev.architectfx.frontend.components.vfx.cells;
+    exports io.github.palexdev.architectfx.frontend.components.vfx.columns;
+    exports io.github.palexdev.architectfx.frontend.components.vfx.rows;
+
     // DI Package
     exports io.github.palexdev.architectfx.frontend.di;
 
+    // Enums package
+    exports io.github.palexdev.architectfx.frontend.enums;
+
     // Events Package
     exports io.github.palexdev.architectfx.frontend.events;
+
+    // Model Package
+    exports io.github.palexdev.architectfx.frontend.model;
+
+    // Settings Package
+    exports io.github.palexdev.architectfx.frontend.settings;
+
+    // Theming Package
+    exports io.github.palexdev.architectfx.frontend.theming;
+
+    // Utils
+    exports io.github.palexdev.architectfx.frontend.utils;
+    exports io.github.palexdev.architectfx.frontend.utils.ui;
 
     // Views Package
     exports io.github.palexdev.architectfx.frontend.views;
