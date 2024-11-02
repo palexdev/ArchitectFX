cf50db865649467f41f671f47ed16b2fd5ade14b
palexdev <alessandro.parisi406@gmail.com>
2024-11-02 21:58:11 +0100

    [frontend] :recycle: Let Gradle built the assets zip

--- modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/theming/AppTheme.java
+++ modules/frontend/src/main/java/io/github/palexdev/architectfx/frontend/theming/AppTheme.java
@@ -18,17 +18,11 @@
 
 package io.github.palexdev.architectfx.frontend.theming;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import io.github.palexdev.architectfx.frontend.Resources;
 import io.github.palexdev.mfxcomponents.theming.base.Theme;
-import org.tinylog.Logger;
 
 public enum AppTheme implements Theme {
     DEFAULT("css/AppTheme.css"),
@@ -52,75 +46,7 @@ public enum AppTheme implements Theme {
 
     @Override
     public InputStream assets() {
-        // FIXME this is to speed things up, but ideally the ZIP should be pre-built
-        ByteArrayOutputStream boas = new ByteArrayOutputStream();
-        try (ZipOutputStream zos = new ZipOutputStream(boas)) {
-            String[] assets = new String[]{
-                "css/Overrides.css",
-                "css/components/Components.css",
-                "css/components/ComboBox.css",
-                "css/components/FileInput.css",
-                "css/dialogs/Dialogs.css",
-                "css/dialogs/ProgressDialog.css",
-                "css/fonts/Fonts.css",
-                "css/fonts/montserrat/Montserrat.css",
-                "css/fonts/montserrat/Montserrat-Black.ttf",
-                "css/fonts/montserrat/Montserrat-BlackItalic.ttf",
-                "css/fonts/montserrat/Montserrat-Bold.ttf",
-                "css/fonts/montserrat/Montserrat-BoldItalic.ttf",
-                "css/fonts/montserrat/Montserrat-ExtraBold.ttf",
-                "css/fonts/montserrat/Montserrat-ExtraBoldItalic.ttf",
-                "css/fonts/montserrat/Montserrat-ExtraLight.ttf",
-                "css/fonts/montserrat/Montserrat-ExtraLightItalic.ttf",
-                "css/fonts/montserrat/Montserrat-Italic.ttf",
-                "css/fonts/montserrat/Montserrat-Light.ttf",
-                "css/fonts/montserrat/Montserrat-LightItalic.ttf",
-                "css/fonts/montserrat/Montserrat-Medium.ttf",
-                "css/fonts/montserrat/Montserrat-MediumItalic.ttf",
-                "css/fonts/montserrat/Montserrat-Regular.ttf",
-                "css/fonts/montserrat/Montserrat-SemiBold.ttf",
-                "css/fonts/montserrat/Montserrat-SemiBoldItalic.ttf",
-                "css/fonts/montserrat/Montserrat-Thin.ttf",
-                "css/fonts/montserrat/Montserrat-ThinItalic.ttf",
-                "css/views/Views.css",
-                "css/views/InitView.css"
-            };
-
-            zos.putNextEntry(new ZipEntry("components/"));
-            zos.closeEntry();
-            zos.putNextEntry(new ZipEntry("dialogs/"));
-            zos.closeEntry();
-            zos.putNextEntry(new ZipEntry("fonts/"));
-            zos.closeEntry();
-            zos.putNextEntry(new ZipEntry("fonts/montserrat/"));
-            zos.closeEntry();
-            zos.putNextEntry(new ZipEntry("views/"));
-            zos.closeEntry();
-
-            for (String asset : assets) {
-                try (InputStream is = Resources.loadStream(asset)) {
-                    if (is != null) {
-                        zos.putNextEntry(new ZipEntry(asset.replace("css/", "")));
-                        byte[] buffer = new byte[1024];
-                        int bytesRead;
-                        while ((bytesRead = is.read(buffer)) != -1) {
-                            zos.write(buffer, 0, bytesRead);
-                        }
-                        zos.closeEntry();
-                    } else {
-                        Logger.error("Could not load asset: {}", asset);
-                    }
-                } catch (NullPointerException ex) {
-                    Logger.error("Could not load asset: {}\n{}", asset, ex.getMessage());
-                }
-            }
-        } catch (IOException ex) {
-            Logger.error(ex.getMessage());
-            return null;
-        }
-
-        // Convert to ByteArrayInputStream after the ZipOutputStream is fully closed
-        return new ByteArrayInputStream(boas.toByteArray());
+        return Resources.loadStream("assets/assets.zip");
     }
 
     @Override
